48986748f1b19a4dfdcb7b0d74092a7ce1498139
Somethong <121753192+Entersjkhdfkjdhfksjf@users.noreply.github.com>
2026-07-21 17:31:00 +0530

    Enhance QuickSettingsWindow with theme buttons
    Added new buttons for theme selection and a wireframe button to the QuickSettingsWindow class.

--- apps/panel/windows/QuickSettingsWindow.h
+++ apps/panel/windows/QuickSettingsWindow.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <libsettings/Setting.h>
+#include <libutils/OwnPtr.h>
+#include <libwidget/Button.h>
 #include <libwidget/Window.h>
 
 namespace panel
@@ -9,9 +12,24 @@ class QuickSettingsWindow : public Window
 {
 public:
     static constexpr int WIDTH = 320;
-    static constexpr int HEIGHT = 320;
 
     QuickSettingsWindow();
+
+private:
+    Button *_wireframe_button = nullptr;
+
+    Button *_theme_skift_dark = nullptr;
+    Button *_theme_skift_light = nullptr;
+    Button *_theme_ayu_dark = nullptr;
+    Button *_theme_ayu_light = nullptr;
+    Button *_theme_solarized_dark = nullptr;
+
+    OwnPtr<settings::Setting> _wireframe_setting;
+    OwnPtr<settings::Setting> _theme_setting;
+
+    void layout_window();
+
+    void update_theme_buttons(const String &active_theme);
 };
 
 } // namespace panel
