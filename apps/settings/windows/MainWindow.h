28bf1cea5782cce34e6363f1d876366a459453b7
Somethong <121753192+Entersjkhdfkjdhfksjf@users.noreply.github.com>
2026-07-25 18:15:44 +0530

    Update MainWindow.h

--- apps/settings/windows/MainWindow.h
+++ apps/settings/windows/MainWindow.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <libutils/String.h>
+#include <libutils/Vector.h>
+#include <libwidget/Button.h>
+#include <libwidget/Container.h>
 #include <libwidget/Window.h>
 
 namespace settings
@@ -7,8 +11,22 @@ namespace settings
 
 class MainWindow : public Window
 {
+private:
+    Container *_content = nullptr;
+
+    Button *_back_button = nullptr;
+    Button *_forward_button = nullptr;
+
+    Vector<String> _history{};
+    int _history_index = -1;
+
 public:
     MainWindow();
+
+    void navigate(String page);
+
+private:
+    void show_page(String page);
 };
 
 } // namespace settings
