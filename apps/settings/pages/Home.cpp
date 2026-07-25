e2550051b2e92fcf26e18fff7bee1a08418a8ef9
Somethong <121753192+Entersjkhdfkjdhfksjf@users.noreply.github.com>
2026-07-25 18:17:07 +0530

    Update Home.cpp

--- apps/settings/pages/Home.cpp
+++ apps/settings/pages/Home.cpp
@@ -1,28 +1,31 @@
+#include <libsystem/process/Process.h>
+
 #include "settings/pages/Home.h"
 #include "settings/widgets/Link.h"
+#include "settings/windows/MainWindow.h"
 
 namespace settings
 {
-    
-HomePage::HomePage(Widget *parent)
+
+HomePage::HomePage(Widget *parent, MainWindow *window)
     : Container(parent)
 {
     layout(STACK());
     flags(Widget::FILL);
 
     auto links = new Container(this);
-    links->layout(GRID(6, 4, 8, 8));
+    links->layout(GRID(4, 4, 8, 8));
     links->insets(16);
 
-    new Link(links, Icon::get("home"), "test");
-    new Link(links, Icon::get("home"), "test");
-    new Link(links, Icon::get("home"), "test");
-    new Link(links, Icon::get("home"), "test");
-    new Link(links, Icon::get("home"), "test");
-    new Link(links, Icon::get("home"), "test");
-    new Link(links, Icon::get("home"), "test");
-    new Link(links, Icon::get("home"), "test");
-    new Link(links, Icon::get("home"), "test");
+    auto appearance_link = new Link(links, Icon::get("palette"), "Appearance");
+    appearance_link->on(Event::ACTION, [window](auto) {
+        window->navigate("appearance");
+    });
+
+    auto about_link = new Link(links, Icon::get("information"), "About");
+    about_link->on(Event::ACTION, [](auto) {
+        process_run("about", nullptr);
+    });
 }
 
 } // namespace settings
