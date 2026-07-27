d4eb89d4d4f20f2348e05a89a97bb1f881e9e579
Somethong <121753192+Entersjkhdfkjdhfksjf@users.noreply.github.com>
2026-07-27 17:39:59 +0530

    Update Appearance.h

--- apps/settings/pages/Appearance.h
+++ apps/settings/pages/Appearance.h
@@ -22,14 +22,20 @@ private:
     Button *_theme_dark = nullptr;
     Button *_theme_light = nullptr;
 
+    Button *_transparency_none = nullptr;
+    Button *_transparency_slight = nullptr;
+    Button *_transparency_full = nullptr;
+
     Vector<Button *> _wallpaper_buttons{};
     Vector<String> _wallpaper_paths{};
 
     OwnPtr<settings::Setting> _wireframe_setting;
     OwnPtr<settings::Setting> _theme_setting;
+    OwnPtr<settings::Setting> _transparency_setting;
     OwnPtr<settings::Setting> _wallpaper_setting;
 
     void update_theme_buttons(const String &active_theme);
+    void update_transparency_buttons(const String &active_mode);
     void update_wallpaper_buttons(const String &active_wallpaper);
 };
 
