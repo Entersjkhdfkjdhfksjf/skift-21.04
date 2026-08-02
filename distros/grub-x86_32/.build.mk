2916b7a75d62c12a336666331277c46c8e308a9a
Somethong <121753192+Entersjkhdfkjdhfksjf@users.noreply.github.com>
2026-08-02 22:01:08 +0530

    Update .build.mk

--- distros/grub-x86_32/.build.mk
+++ distros/grub-x86_32/.build.mk
@@ -1,9 +1,11 @@
-$(BOOTDISK): $(RAMDISK) $(KERNEL_BINARY) $(DISTRO_DIRECTORY)/grub.cfg
+$(BOOTDISK): $(RAMDISK) $(KERNEL_BINARY) $(DISTRO_DIRECTORY)/grub.cfg $(wildcard $(DISTRO_DIRECTORY)/theme/*)
 	$(DIRECTORY_GUARD)
 	@echo [GRUB-MKRESCUE] $@
 
 	@mkdir -p $(BOOTROOT)/boot/grub
+	@mkdir -p $(BOOTROOT)/boot/grub/themes/amberos
 	@cp $(DISTRO_DIRECTORY)/grub.cfg $(BOOTROOT)/boot/grub/
+	@cp $(DISTRO_DIRECTORY)/theme/* $(BOOTROOT)/boot/grub/themes/amberos/
 	@gzip -c $(RAMDISK) > $(BOOTROOT)/boot/ramdisk.tar.gz
 	@gzip -c $(KERNEL_BINARY) > $(BOOTROOT)/boot/kernel.bin.gz
 
